ab2cec0014cf917e7c0a290cc7ce7ff7d7d43002
Don Vaughn <don@vaughnline.com>
2020-06-25 11:05:17 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { StartComponent } from './start/start.component';
+import { AppRoutingModule } from './app-routing.module';
+import { demoComponents } from './demo/demo-definitions';
+import { StartseiteComponent } from './startseite/startseite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent,
-    StartComponent
+    // TODO: 6) Demo Components einbinden
+    ...demoComponents,
+    StartseiteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // TODO: 7) bei Angular 8 muss man eventuell entryComponents definieren/einbinden
+  entryComponents: []
 })
 export class AppModule { }
+
+
+// TODO: 8) jetzt kommt man in Development an localhost:4200/demo -> ng serve
+// TODO: 9) unter Production ist er nicht erreichbar -> ng serve --prod
+// TODO: 10) beim Bauen in Development mode ist Demo Code drin (TODO 10b anschauen) -> ng build && npm run break-if-demo-found
+// TODO: 11) beim Bauen in Production mode ist Demo Code weg -> ng build --prod && npm run break-if-demo-found
