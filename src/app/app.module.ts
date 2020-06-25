6e62ff256135e6fe6da849856568516351997a83
Don Vaughn <donald.vaughn@silbury.com>
2020-06-25 10:55:12 +0200

    Added demo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
