4984885e64c5e2890ff5473659eb6eba8b6e9154
Don Vaughn <don@vaughnline.com>
2020-06-25 11:06:42 +0200

    Create demo-definitions.ts

---
+++ src/app/demo/demo-definitions.ts
@@ -0,0 +1,12 @@
+// TODO: 1) eine Datei definieren, die beim "Replace
+
+import { DemoComponent } from './demo.component';
+
+export const demoComponents = [DemoComponent];
+
+export const demoRoutes = [
+  {
+    path: 'demo',
+    component: DemoComponent,
+  },
+];
